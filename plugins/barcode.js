b4d1fac8e7a2fd622c1a0d5f33e475ff04d69e49
Ayishu74 <100610655+Ayishu74@users.noreply.github.com>
2022-02-28 23:03:57 +0530

    Update barcode.js

--- plugins/barcode.js
+++ plugins/barcode.js
@@ -1,5 +1,5 @@
-/*coded by ameer-kallumthodi
-https://youtube.com/channel/UCpGa88rhUFYj-6-LBqBbWKw
+/*coded by ꜰᴀꜱɪʟ
+https://youtube.com/c/AFTROLLSS
 */
 
 const Asena = require('../events');
@@ -17,7 +17,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://docs-jojo.herokuapp.com/api/barcode_maker?text=${match[1]}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by PIKACHU' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Ayishu-Mwol_V2' })
 
     }));
 }
