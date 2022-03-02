3c495c1db2b492c58ad6bd0d66019e6621c4697b
Ayishu-Mwol_V2 <100714787+Ayisha74@users.noreply.github.com>
2022-03-02 11:50:02 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*ᴀʏɪꜱʜᴜ-ᴍᴡᴏʟ_ᴠ2 bot created by ꜰᴀꜱɪʟ*
-*Creator number : wa.me/919947430792?text=Hi%20Ameer%20Suhail.%20*
+*Creator number : wa.me/919947430792?text=Hi%20Fasil%20Ayishubot.%20*
 
 *𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : https://youtube.com/c/AFTROLLSS* 
 
