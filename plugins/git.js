c12c57ab56a4aceb168d3b1c92bc8b5ddac93179
Ayishu-Mwol_V2 <100714787+Ayisha74@users.noreply.github.com>
2022-03-02 17:27:20 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,16 +7,16 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/d4DIca6.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*ᴀʏɪꜱʜᴜ-ᴍᴡᴏʟ_ᴠ2 bot created by ꜰᴀꜱɪʟ*
 *Creator number : wa.me/919947430792?text=Hi%20Fasil%20Ayisha.%20*
 
 *𝐁𝐨𝐭 𝐎𝐰𝐧𝐞𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 : https://youtube.com/c/AFTROLLSS* 
 
-*Githublink (Setup)  :    https://github.com/Ayishu74/Ayishu-Mwol_V2*
+*Githublink (Setup)  :    https://github.com/Ayisha74/Ayishu-Mwol_V2*
 
-*Audio commads :   https://github.com/Ayishu74/Ayishu-Mwol_V2/tree/master/uploads*
+*Audio commads :   https://github.com/Ayisha74/Ayishu-Mwol_V2/tree/master/uploads*
 
 *Sticker commads : https://github.com/Ayishu74/Ayishu-Mwol_V2/tree/master/uploads*
 
